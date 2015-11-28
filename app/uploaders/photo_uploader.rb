557866cb99e0e14713ec2e22d4cd11c9ce0bf1bf
Alex <babakhanov1@gmail.com>
2015-11-28 12:56:58 +0200

    filte uploader

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,39 @@
+class PhotoUploader < CarrierWave::Uploader::Base
+
+  include CarrierWave::MiniMagick
+
+  def filename
+    "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :large do
+    resize_to_limit(800, 800)
+  end
+
+  version :big do
+    resize_to_limit(420, 420)
+  end
+
+  version :medium do
+    resize_to_fit(320, 320)
+  end
+
+  version :micro do
+    resize_to_fit(60, 60)
+  end
+
+  def extension_white_list
+    %w(jpg jpeg png)
+  end
+
+  protected
+
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+  end
+end
