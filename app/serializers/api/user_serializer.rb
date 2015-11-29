24ffeee4786737d463992c49b33cf0ab8d5feba1
Alex <babakhanov1@gmail.com>
2015-11-29 10:47:29 +0200

    user settings

--- app/serializers/api/user_serializer.rb
+++ app/serializers/api/user_serializer.rb
@@ -2,6 +2,10 @@ class Api::UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :about, :photos, :email, :id, :avatar
 
   def avatar
-    object.avatar
+    if object.avatar
+      object.avatar.medium.url
+    else
+      nil
+    end
   end
 end
